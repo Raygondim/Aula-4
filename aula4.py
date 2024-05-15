a4b330c3069deccf38a4bbb4324a3d831d4bf014
Raygondim <rayssagondim@outlook.com>
2024-05-15 15:22:00 -0300

    Add files via upload

---
+++ aula4.py
@@ -0,0 +1,75 @@
+# Exercícios: 
+
+# 1 Imprima uma mensagem de boas-vindas na tela.
+
+print('Seja bem vindo(a)!')
+
+# 2 Declare uma variável booleana verdadeiro
+# com o valor True e imprima seu tipo
+
+booleana = True
+print(type(booleana))
+
+# 3 Imprima o resultado da multiplicação de dois números decimais de sua escolha
+
+print(1.2 * 3.4)
+
+# 4 Imprima o resultado da divisão de dois números inteiros 
+# de sua escolha.
+
+print(4//2)
+
+# 5 Imprima o resultado da subtração de dois números inteiros de sua escolha
+
+print(10-5)
+
+# 6 Imprima o resultado da divisão inteira de dois números inteiros 
+# de sua escolha.
+
+print(10//2)
+
+# 7 Imprima o resultado da multiplicação de dois números decimais de sua escolha
+
+print(3.5*9.6)
+
+# 8 Declare uma variável numero e atribua um número inteiro. Em seguida, 
+# imprima o dobro desse número
+
+numero = 10
+print(numero ** 2)
+
+# 9 Crie uma calculadora de soma com as ferramentas que vc já 
+# conhece(Use apenas input, print e variavel)
+
+# numero1 = float(input('Digite um número '))
+# numero2 = float(input('Digite outro número '))
+
+# print(f'''
+# Resultado da soma: {numero1+numero2}
+# Resultado da subtração: {numero1-numero2}
+# Resultado da multiplicação: {numero1*numero2}
+# Resultado da divisão: {numero1//numero2}
+# '''
+# )
+
+# 10 Crie um sistema de cadastro com as estruturas que vc já 
+# conhece(Use apenas input, print e variavel)
+
+print('Crie seu cadastro')
+
+nome = input('Digite seu nome: ')
+sobrenome = input('Digite seu sobrenome: ')
+data = input('Digite sua data de nascimento: ')
+email = input('Digite seu email: ')
+altura = input('DIgite sua altura: ')
+
+print(f'''
+Nome e sobrenome: {nome+sobrenome}
+Data de nascimento: {data}
+Email: {email}
+Altura: {altura}
+'''
+)
+
+print('CADASTRO CONCLUIDO')
+
